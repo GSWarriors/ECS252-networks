178aee3df5d8813208b9ff1e21ca098d79ec3aa5
Siddartha P <sid.punj@gmail.com>
2021-04-28 11:30:02 -0700

    Update server file to keep preforked children open

--- project1-ethernet simulation/server.py
+++ project1-ethernet simulation/server.py
@@ -1,53 +1,52 @@
 # simple (non-concurrent) TCP server example
 from socket import *
 import os 
+import signal
+
+
+
+def new_host(i, listeningSocket):
+    pid = os.fork()
+  
+
+    if pid > 0:
+        print("parent process, returning back to loop")
+        return pid
+    else:
+        print("created new child process with id: " + str(os.getpid()))
+        while 1:
+            child_connectionsocket, addr = listeningSocket.accept()
+            #handle the incoming request
+            print("handling incoming request from client with host: " + str(i))
+
+            #close child connection after its handled       
+            child_connectionsocket.close()
+
+
+
 
 
 def main():
-    serverPort = 12001
+    serverPort = 12000
     listeningSocket = socket(AF_INET, SOCK_STREAM)
     print(listeningSocket)
     listeningSocket.bind(('', serverPort))
     listeningSocket.listen(1)
     print('Server ready, socket', listeningSocket.fileno(), 'listening on localhost :', serverPort)
 
-    num_hosts = 10
-    count = 0
+    num_hosts = 30
 
-    while 1:
-        connectionSocket, addr = listeningSocket.accept()
-        pid = os.fork()
+    #don't need to have parent in while loop, just needs to sleep.
+    #the preforked children handle the connections
 
-        if pid > 0:
-            print("i am the parent process.")
-            print("process ID: " + str(os.getpid()))
-            print()
-        
-        else:
-            print("i am child process.")
-            print("process ID: " + str(os.getpid()))
-            print()
-            listeningSocket.close()
-            print("handling incoming request from connected socket")
-            os._exit(0)
-        
+    for i in range(0, num_hosts):
+        new_host(i, listeningSocket)
 
-        print("closing connected socket.")
-        connectionSocket.close()
+    signal.pause()
 
 
 
 main()
-
-
-
-
-        #firstName = connectionSocket.recv(1024)
-        #print('Got connection on socket', connectionSocket.fileno(), 'from', addr[0], ':', addr[1])
-        #connectionSocket.send(str.encode('Hi ' + bytes.decode(firstName) + '!'))
-        #lastName = connectionSocket.recv(1024)
-        #connectionSocket.send(str.encode('Hello ' + bytes.decode(firstName) + ' ' + bytes.decode(lastName)+ '!'))
-        #print('Closing socket', connectionSocket.fileno())
 
 
 
