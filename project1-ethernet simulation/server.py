82c251dc55941720e2a4a0f557e2d6c4edbc87f9
Siddartha Punj <sid.punj@gmail.com>
2021-05-10 13:37:39 -0700

    Started adding p-aloha transmission if all unique processes

--- project1-ethernet simulation/server.py
+++ project1-ethernet simulation/server.py
@@ -44,70 +44,110 @@ class Server_Process(object):
             yield self.env.timeout(1)
 
             print("current time: " + str(self.env.now))
+
+
             interrupt_list = []
             #check here if any of the process queues have a packet that just arrived in the last slot 
             for key, val in self.process_dict.items():
-                
+
+                print("process: " + str(key))
+
                 for packet in val[1]:
                     if self.env.now - packet.arrival_time <= 1:
+                        print("arrival time: " + str(packet.arrival_time))
+
                         if key not in interrupt_list:
                             interrupt_list.append((key, val[0]))
-                            print("a packet in process " + str(key) + " has just arrived, servicing.")
-                            print("the arrival time: " + str(packet.arrival_time))
-                            print()
-                        else:
-                            print("packet from same process " + str(key) + " has arrived.")
+                            #print("a packet in process " + str(key) + " has just arrived, servicing.")
+                            #print("the arrival time: " + str(packet.arrival_time))
+                            #print()
+                        #else:
+                            #print("packet from same process " + str(key) + " has arrived.")
+                print()
              
 
-            print("total packets that arrived in this interval: " + str(len(interrupt_list)))
-            print()
+            
 
             #check for collision and remove that packet from queues of both processes
+            #if we actually have a collision
+            process_freq = {}
 
             if len(interrupt_list) >= 2:
-                for i in range(0, len(interrupt_list)):
-                    curr_queue = self.process_dict[interrupt_list[i][0]][1]
+                print("interrupt list: " + str(interrupt_list))
 
-                    removed_packet = curr_queue.pop()
-                    print("removed packet from process " + str(interrupt_list[i][0]) + " with arrival time: " + 
-                    str(removed_packet.arrival_time))
+                for index, tuple in enumerate(interrupt_list):
+                    key = tuple[0]
+                    reference = tuple[1]
+                    
+                    if key not in process_freq:
+                        process_freq[key] = 1
+                    else:
+                        process_freq[key] += 1
+            
+                can_interrupt = self.check_processes(process_freq)
 
-                #for i in range(0, len(interrupt_list)):
-                    #interrupt_list[i][1].interrupt()
+
+
+                if can_interrupt:
+                    for i in range(0, len(interrupt_list)):
+                        curr_queue = self.process_dict[interrupt_list[i][0]][1]
+                        removed_packet = curr_queue.pop()
+                        print("removed packet from process " + str(interrupt_list[i][0]) + " with arrival time: " + 
+                        str(removed_packet.arrival_time))
+                        interrupt_list[i][1].interrupt()
 
             
             self.server_busy = False 
 
+    
+
+        
+    def check_processes(self, process_freq):
+
+        check_val = 1
+        can_interrupt = True
+
+        for val in process_freq.values():
+            if val != check_val:
+                can_interrupt = False 
+        
+        #if can_interrupt:
+        #    print("we can interrupt these processes")
+        #else:
+        #    print("can't interrupt processes. they have at least one of the same process listed twice")
+        
+        return can_interrupt
 
 
 
 
 
-            #queue for current process
-            """while len(queue) > 0:
+    #queue for current process
+    """while len(queue) > 0:
 
-                packet = queue.pop(0)
-                print("the packet number: " + str(packet.identifier))
-                print("the arrival time: " + str(packet.arrival_time))
-                print()
-                
-                yield self.env.timeout(random.expovariate(G.MU))
+        packet = queue.pop(0)
+        print("the packet number: " + str(packet.identifier))
+        print("the arrival time: " + str(packet.arrival_time))
+        print()
+        
+        yield self.env.timeout(random.expovariate(G.MU))
 
-            #we have emptied the queue for this process
-            server_busy = False
-            print("queue emptied for process: " + str(i))"""
+    #we have emptied the queue for this process
+    server_busy = False
+    print("queue emptied for process: " + str(i))"""
 
 
 
-            """for key, val in self.process_dict.items():
-                print("process: " + str(key))
-                
-                for packet in val[1]:
-                    print("arrival time: " + str(packet.arrival_time))
+    """for key, val in self.process_dict.items():
+        print("process: " + str(key))
+        
+        for packet in val[1]:
+            print("arrival time: " + str(packet.arrival_time))
 
-                print()
+        print()
 
-            print()"""
+    print()"""
+
                 
 
 
@@ -146,30 +186,38 @@ class Arrival_Process(object):
                 else:
                     self.server_process.process_dict[i][1].append(new_packet)
 
-                #print("the queue size for process " + str(i) + " is now: " + str(len(self.server_process.process_dict[i][1])))
-                #print()
-
 
                 #check whether server busy to start transmitting packets
                 if self.server_process.server_busy == False:
                     self.server_process.server_busy = True
                     #wait for some time in order to let the process yield again
-                    
+
+
+
+
             except simpy.Interrupt:
-                print("process " + str(i) + " has a collision, need to resend packet.")
-                print()
 
-                """retransmit = random.randint(0, 1)
+                #retransmit = random.randint(0, 1)
+                retransmit = random.uniform(0, 1)
 
-                #50% chance of true
-                if retransmit == 1:
+                #retransmit packet for each process with a 50% probability by adding to the queue again 
+                #reduce packet number by 1 because retransmitting that packet
+                if retransmit <= 0.8:
+                    self.packet_number -= 1
+                    arrival_time = self.env.now
                     new_packet = Packet(self.packet_number,arrival_time)
+                    print("collision packet for process: " + str(i) + " has been resent with arrival time: " 
+                    + str(arrival_time))
                     
                     #add to queue again
                     if not self.server_process.process_dict[i][1]:
                         self.server_process.process_dict[i][1] = [new_packet]
                     else:
-                        self.server_process.process_dict[i][1].append(new_packet)"""
+                        self.server_process.process_dict[i][1].append(new_packet)
+                
+                #check whether server busy to start transmitting packets
+                if self.server_process.server_busy == False:
+                    self.server_process.server_busy = True
 
 
 
